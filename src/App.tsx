ccd67e730566eadbe19041e94c30d8095431d3f0
Sibz <sibabalwedes@gmail.com>
2025-10-24 15:15:11 +0200

    Add useTheme hook to App component

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ import NotFound from "./pages/NotFound";
 const queryClient = new QueryClient();
 
 const App = () => (
+  useTheme();
+
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
